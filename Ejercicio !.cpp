a9d4d0cec28244889383bea527cb7e54516694f1
ejz95 <67702602+ejz95@users.noreply.github.com>
2020-07-05 00:30:42 -0600

    Add files via upload

---
+++ Ejercicio !.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hola mundo" << endl;
+    
+    
+    
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ cpp/C++/Ejercicio 2.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   string a;
+   cout << "Ingrese su Nombre" << endl;
+   cin >> a;
+   cout << endl;
+   cout << "Su Nombre es: " << a << endl;
+   
+   
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
