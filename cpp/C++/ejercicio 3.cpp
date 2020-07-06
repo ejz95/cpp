75a6fa6b37662923b6c453aa58b0b6f83bcbd934
ejz95 <67702602+ejz95@users.noreply.github.com>
2020-07-05 18:16:10 -0600

    Add files via upload

---
+++ cpp/C++/ejercicio 3.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int numb;
+    numb=15;
+    cout<< numb << endl;
+    
+    numb=12;
+    cout<< "entero modificado: " << numb << endl;
+    
+    
+    
+    
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
