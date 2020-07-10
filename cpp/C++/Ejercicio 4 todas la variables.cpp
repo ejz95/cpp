12b3c27350f0ea5945c3b547b52a288e90250ac3
ejz95 <67702602+ejz95@users.noreply.github.com>
2020-07-10 12:04:29 -0600

    Add files via upload

---
+++ cpp/C++/Ejercicio 4 todas la variables.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   bool valorboolean;
+   valorboolean=true;
+   int valorent=15;
+   double valordoub=20.99;
+   string valorstring="hola como estan" ;
+   char valorchar = 0;
+   
+   cout << "valor Boolean " << valorboolean <<endl;
+   cout << "valor Entero "<< valorent<<endl;
+   cout<<"valor Double " << valordoub<< endl;
+   cout << "valor string " <<valorstring<< endl;
+   cout << "valor Char " <<valorchar<< endl; 
+   
+   
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ cpp/C++/Ejercicio 5 calculadora.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a,b,resultado;
+    a=0,b=0;
+    
+    cout<<"ingrese el valor de A: ";
+    cin>>a ;
+    cout<<"Ingrese el valor de B: ";
+    cin>> b;
+    resultado=a+b;
+    cout<< "El resultado de A+B es: "<< resultado<<endl;
+    
+    
+    
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
