7b45021ce11c68f8886a341900967c26aaceaa71
ejz95 <67702602+ejz95@users.noreply.github.com>
2020-08-01 16:13:21 -0600

    Add files via upload

---
+++ cpp/C++/Ejercicio 45 Tarea 6/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+extern void menu();
+int main(int argc, char** argv)
+ {
+	menu();
+	
+	
+	
+system("pause");
+}
